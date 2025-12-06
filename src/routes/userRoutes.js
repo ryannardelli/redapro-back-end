47c643a212b2ec34a03dd09d3875d194fe4cb31f
Ryan Nardelli <ryannardelli12@gmail.com>
2025-12-06 20:02:53 -0300

    chore: Added swagger on apllication

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,39 @@ const router = express.Router();
 
 const userController = require("../controllers/User");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Operações relacionadas a usuários
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Cria um novo usuário
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso
+ *       400:
+ *         description: Erro na requisição
+ */
+
 router.post("/", userController.create);
 
 module.exports = router;
