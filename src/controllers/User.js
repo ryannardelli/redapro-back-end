234a96f145f2da2cfbe2c3e802e14b3f7b5845b9
Ryan Nardelli <ryannardelli12@gmail.com>
2025-12-06 23:27:22 -0300

    refactor: Fixes mad eon organizations of folders

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,28 +1,14 @@
 const User = require("../models/user");
 
-const InvalidEmailError = require("../exceptions/domain/user/InvalidEmailError");
-const InvalidPasswordError = require("../exceptions/domain/user/InvalidPasswordError ");
-const EmailAlreadyExistsError = require("../exceptions/domain/user/EmailAlreadyExistsError");
-const InvalidNameError = require("../exceptions/domain/user/InvalidNameError");
+const userService = require("../services/userService/createUser");
 
 module.exports = {
-    async create(req, res, next) {
-        try {
-            const { name, email, password } = req.body;
-
-            if(password.length < 8 || password.length > 20) throw new InvalidPasswordError();
-
-            if (!/\S+@\S+\.\S+/.test(email)) throw new InvalidEmailError();
-            
-            const existingEmail = await User.findOne({ where: { email } });
-            if(existingEmail) throw new EmailAlreadyExistsError();
-
-            if(name.length < 2 || name.length > 100) throw new InvalidNameError;
-
-            await User.create({ name, email, password });
-            res.status(201).json({ message: "Usuário criado com sucesso!" });
-        } catch(error) {
-            next(error);
-        }
+  async create(req, res, next) {
+    try {
+      const user = await userService.createUser(req.body);
+      res.status(201).json({ message: "Usuário criado com sucesso!", user });
+    } catch (error) {
+      next(error);
     }
-}
+  }
+};
---
+++ src/tests/mock/user.js
@@ -0,0 +1,6 @@
+// dados de teste e comportamento
+module.exports = {
+  validUser: { name: "Ryan", email: "r@test.com", password: "12345678" },
+  invalidEmail: { name: "Ryan", email: "invalid", password: "12345678" },
+  shortPassword: { name: "Ryan", email: "r@test.com", password: "123" },
+};
