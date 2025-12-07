c618d2ae4b8390c478f376918dce52b845fa1ab1
Ryan Nardelli <ryannardelli12@gmail.com>
2025-12-07 00:27:04 -0300

    test: New test unit to userService

--- src/tests/mock/user.js
+++ src/tests/mock/user.js
@@ -1,6 +1,17 @@
-// dados de teste e comportamento
+// Dados de teste
+const validUser = { name: "Ryan", email: "r@test.com", password: "12345678" };
+const invalidEmail = { name: "Ryan", email: "invalid", password: "12345678" };
+const shortPassword = { name: "Ryan", email: "r@test.com", password: "123" };
+
+// Funções mock do model
+const UserMock = {
+  findOne: jest.fn(),   // simula busca no banco
+  create: jest.fn()     // simula criação no banco
+};
+
 module.exports = {
-  validUser: { name: "Ryan", email: "r@test.com", password: "12345678" },
-  invalidEmail: { name: "Ryan", email: "invalid", password: "12345678" },
-  shortPassword: { name: "Ryan", email: "r@test.com", password: "123" },
+  validUser,
+  invalidEmail,
+  shortPassword,
+  UserMock
 };
