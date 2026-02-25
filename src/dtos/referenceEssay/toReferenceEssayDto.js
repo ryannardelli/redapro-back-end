dbeb800c01667b5bb0a2f1372af3af8eaf74a3aa
Ryan Nardelli <ryannardelli12@gmail.com>
2026-02-25 02:05:41 -0300

    new fix made into back-end dto references essays

--- src/dtos/referenceEssay/toReferenceEssayDto.js
+++ src/dtos/referenceEssay/toReferenceEssayDto.js
@@ -5,7 +5,13 @@ function toReferenceEssayDto(essay) {
         content: essay.content,
         year: essay.year,
         pdf_url: essay.pdf_url,
-        categoryId: essay.categoryId ?? null,
+
+        category: essay.category ? {
+            id: essay.category.id,
+            name: essay.category.name,
+            description: essay.category.description
+        } : null,
+        
         createdAt: essay.createdAt ?? null,
         updatedAt: essay.updatedAt ?? null
     }
