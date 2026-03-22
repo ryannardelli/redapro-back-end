138f1542aed5d5b7727d6881d45e95e2bdb820a4
Ryan Nardelli <ryannardelli12@gmail.com>
2026-03-22 03:01:25 -0300

    feat: New fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,29 +1,20 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         dialect: 'postgres',
-//         logging: false,
-//     }
-// )
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 5432,
+    dialect: 'postgres',
     dialectOptions: {
-      socketPath: process.env.DB_HOST,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
+    logging: false,
   }
 );
 
