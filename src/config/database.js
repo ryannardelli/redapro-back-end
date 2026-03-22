9a19c87d7f4cc6c6cf20fc250490689c843085fc
Ryan Nardelli <ryannardelli12@gmail.com>
2026-03-22 02:29:35 -0300

    feat: New fix

--- src/config/database.js
+++ src/config/database.js
@@ -13,15 +13,18 @@ require("dotenv").config();
 //     }
 // )
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  dialect: "postgres",
-  dialectOptions: {
-    socketPath: process.env.DB_HOST,
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      socketPath: process.env.DB_HOST,
+    },
+  }
+);
 
 module.exports = sequelize;
