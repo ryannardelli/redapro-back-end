4b017214f4b3767788e483cfe60a04fdd8db381e
Ryan Nardelli <ryannardelli12@gmail.com>
2026-03-25 17:09:19 -0300

    perf: Was removed comment about githubModels.js, old coode

--- src/config/githubModels.js
+++ src/config/githubModels.js
@@ -1,15 +1,3 @@
-// require("dotenv").config();
-
-// const ModelClient = require("@azure-rest/ai-inference").default;
-// const { AzureKeyCredential } = require("@azure/core-auth");
-
-// const githubModels = ModelClient(
-//   "https://models.github.ai/inference",
-//   new AzureKeyCredential(process.env.TOKEN_GITHUB)
-// );
-
-// module.exports = githubModels;
-
 require("dotenv").config();
 
 const ModelClient = require("@azure-rest/ai-inference").default;
