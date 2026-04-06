c7af48dd221e4055b58c27e3db80fb8462dc1e30
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-06 01:33:23 -0300

    ci/cd: New fix

--- src/config/database.js
+++ src/config/database.js
@@ -10,17 +10,24 @@ dotenv.config({ path: envFile });
 
 let sequelize;
 
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+if (env === "production") {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  });
+    }
+  );
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
