b6df26f3d030065ed2a963730468f0b5b8ce05de
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-06 01:13:23 -0300

    ci/cd: New fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,38 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
-const useSSL = process.env.DB_SSL === "true";
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.dev";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: 5432,
+dotenv.config({ path: envFile });
+
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    dialectOptions: useSSL
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : {},
     logging: false,
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
